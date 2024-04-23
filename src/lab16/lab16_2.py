3f7be5514a0fdb46d81676533fc118bd2726355d
Kiril020230 <kirill.ignatev.oo@gmail.com>
2024-04-23 22:18:57 +0400

    2

--- src/lab16/lab16_2.py
+++ src/lab16/lab16_2.py
@@ -1,14 +1,17 @@
 def fact(n):
-    if n == 0:  return 1
-    else:       return n * fact(n - 1)
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n - 1)
 
-def calculate_expression(n, m):
+def fun(n, m):
     result = fact(n) / (fact(m) * fact(n - m))
     return result
+
 
 n = int(input("Введите значение n: "))
 m = int(input("Введите значение m: "))
 
-c = calculate_expression(n, m)
 
-print("Значение выражения:", c)
+с = fun(n, m)
+print("Значение выражения:", с)
